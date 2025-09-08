2564f1a698e3b110245330d0a5cf9f6b51037bb9
konnak <cleisondecarvalho@hotmail.com>
2025-09-07 22:46:06 -0300

    Fix port to 8080 for Discloud compatibility

--- app.py
+++ app.py
@@ -389,5 +389,5 @@ def generate_report_api():
 
 if __name__ == '__main__':
     # Para Discloud
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(debug=False, host='0.0.0.0', port=port)
